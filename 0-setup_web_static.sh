f4650c1d61bc6cb42e43d502fcd4310b299e25b2
kaybrian <kayonbrian@gmail.com>
2023-06-21 22:36:49 +0200

    setting a webserver

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
-# Configure your Nginx server so that
-apt-get update
-apt-get -y install nginx
 
-directories=("/data/web_static/releases/" "/data/web_static/shared/")
+# Install Nginx if not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
 
-for directory in "${directories[@]}";
-do
-    mkdir -p $directory
-done
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-echo "<html>
+ysudo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" >/data/web_static/releases/test/index.html
+</html>" >  sudo tee /data/web_static/releases/test/index.html
 
-#Create a symbolic link to /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# recursively to all files and directories within the folder
-chown -R ubuntu:ubuntu /data/
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
+# Update Nginx configuration with alias
+sudo sed -i '/^\s*server_name _;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-service nginx restart
+# Restart Nginx
+sudo service nginx restart
+
+# Exit successfully
+exit 0
